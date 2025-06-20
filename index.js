4bf408e15434516c150882527002d90b98d0139b
sharon <njerisharon84@gmail.com>
2025-06-20 14:50:10 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Define num1 and num2 so we can multiply them
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+// Multiply num1 and num2 to get 62
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 to get a remainder of 4
+const num3 = 28;
+const num4 = 6;
+
+// Use the modulo operator to get the remainder
+const mod = num3 % num4;
+
+// Use Math.max to find the highest number in the set
+const max = Math.max(1, 3, 5, 10, 15, 20);
